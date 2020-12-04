4df9f9fcb2b03534a625a2731bcebfd53aaf1d73
Lucas Batista da Silva <lucasbatista605@gmail.com>
2020-12-04 00:43:52 -0300

    init: definindo estrutura de pacotes

---
+++ src/main/java/br/com/transactionstatus/TransactionStatusApplication.java
@@ -0,0 +1,13 @@
+package br.com.transactionstatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionStatusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionStatusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/transactionstatus/TransactionStatusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.transactionstatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionStatusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
